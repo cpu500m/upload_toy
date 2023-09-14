6ab1fc422d48e49aeb7d1998cf10f76019bf38a6
cpu500m <paul108203@naver.com>
2023-09-14 19:35:49 +0900

    Initial commit

---
+++ src/main/java/toy1/upload_toy/UploadToyApplication.java
@@ -0,0 +1,13 @@
+package toy1.upload_toy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadToyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadToyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/toy1/upload_toy/UploadToyApplicationTests.java
@@ -0,0 +1,13 @@
+package toy1.upload_toy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadToyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
